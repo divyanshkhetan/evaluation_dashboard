437ba301f87619621e31052060c24d96bf69b237
Divyansh Khetan <divyanshkhetan18@gmail.com>
2023-11-03 17:41:56 +0530

    build settings update

--- server.js
+++ server.js
@@ -26,5 +26,13 @@ app.use('/auth', require('./routes/auth'));
 app.use('/mentors', require('./routes/mentors'));
 app.use('/students', require('./routes/students'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
 // Listen to port
 app.listen(port, () => console.log(`Port ${port} started`));
